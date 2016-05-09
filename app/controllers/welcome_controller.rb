ac316eefb1375941511ee80019f06d8c2806df88
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-05-09 22:51:43 +1200

    initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ActionController::Base
+  before_action :authenticate_user!
+	def index
+		
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+end
